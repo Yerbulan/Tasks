b540012e07b0dca6e090dbd3bdf7e6da6be9ba54
Yerbulan <bula136@gmail.com>
2023-05-22 15:19:30 +0600

    May 22 2

--- front_end/script.js
+++ front_end/script.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
                 console.log("success"); // another sanity check
             },
 
-            // handle a non-successful response
+            // handle a non-successful response s
             error : function(xhr,errmsg,err) {
                 console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
             }
